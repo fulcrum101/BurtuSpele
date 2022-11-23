b9dd14025941233324c1f214219336b3459fdb0f
mmuffin101 <69482465+fulcrum101@users.noreply.github.com>
2022-11-23 15:26:21 +0200

    fixed shit

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, redirect, url_for, render_template, request
-import requests, json
+import json
+import requests
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -8,15 +9,14 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-# @app.route('/get_word', methods=['GET'])
-# def get_word():
-#     length = request.args.to_dict().get('length', type=int)
-#     query = {'minLength': length,
-#              'maxLength' : length,
-#              'api_key': }
-#     response = requests.get('https://api.wordnik.com/v4/words.json/',
-#                             params=query).json()
-#     return json.dumps(response)
+
+@app.route('/get_word', methods=['GET'])
+def get_word():
+    length = request.args.to_dict()['length']
+    params = {'length': length}
+    response = requests.get('https://random-word-api.herokuapp.com/word', params=params)
+    return json.dumps(response.text)
+
 
 @app.route('/get_rules')
 def get_rules():
@@ -26,5 +26,6 @@ def get_rules():
     return json.dumps(data)
 
 
-
-app.run(debug=True)
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
