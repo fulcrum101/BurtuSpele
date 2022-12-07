dea79ca5f590995002b1e14b5d0d598bf3eb24f9
mmuffin101 <69482465+fulcrum101@users.noreply.github.com>
2022-12-07 15:05:35 +0200

    almost fixed shit lmao

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import requests
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
-
+path = 'assets/leaderbard.json'
 
 @app.route('/')
 def index():
@@ -13,8 +13,9 @@ def index():
 @app.route('/get_word', methods=['GET'])
 def get_word():
     length = request.args.to_dict()['length']
-    params = {'length': length}
-    response = requests.get('https://random-word-api.herokuapp.com/word', params=params)
+    params = {'length': length,
+              'words': 1}
+    response = requests.get('https://random-word-api.vercel.app/api', params=params)
     return json.dumps(response.text)
 
 
@@ -25,6 +26,23 @@ def get_rules():
         data = json.load(f)
     return json.dumps(data)
 
+@app.route('/lb', methods=['GET', 'POST']) # leaderboard
+def lb():
+    resList = []
+    if not os.path.exists(path):
+        with open(path, 'w') as f:
+            json.dump([], f)
+    if request.method == 'POST':
+        dati  = request.json
+        if dati is not None and len(dati)>0:
+            with open(path, 'r') as f:
+                resList = json.load(f)
+                resList.append(dati)
+            with open(path, 'w') as f:
+                json.dump(resList, f, indent=1)
+    with open(path, 'r') as f:
+        resList = json.load(f)
+    return json.dumps(sorted(resList, key=lambda v: v['points'], reverse=True))
 
 if __name__ == '__main__':
     app.debug = True
